e67f6527cf368b23dda2b7f8e59f8904a14334fa
yinzhedy <yinzhe.dy@gmail.com>
2023-06-15 17:23:47 -0400

    further styling for contact page/contact form

--- src/pages/contact/contact-index.js
+++ src/pages/contact/contact-index.js
@@ -3,21 +3,25 @@ const Contact = () => {
         <main class="flex rows">
         <div id="section-container" class="flex column nowrap justify-center align-center">
             <section class = "section flex column nowrap justify-left align-left">
-                <h1>Contact Information and Links</h1>
+                <h1 class="header-1">Contact Information and Links</h1>
                 <a> Github</a>
                 <a>LinkedIn</a>
                 <a>Email: yinzhe.dy@gmail.com</a>
             </section>
-            <section class="section flex column nowrap">
-                <form class ="flex column nowrap justifu-left align-left">
-                  <h1>Contact Form</h1>
-                    <label>Name:</label>
-                    <input type="text"/>
-                    <label>Email:</label>
-                    <input type="email"/>
-                    <label>Message:</label>
-                    <input type="text"/>
-                    <input id="contact-submit-button" type="submit"/>  
+            <section id="contact-form-section" class="section flex column nowrap">
+                <form id= "contact-form" class ="flex column nowrap justify-center align-center">
+                  <h1 class="header-1">Contact Form</h1>
+                    <label class= "contact-form-label">Name:</label>
+                    <input class = "contact-form-input" type="text"/>
+
+                    <label class="contact-form-label">Email:</label>
+                    <input class = "contact-form-input" type="email"/>
+
+                    <label class="contact-form-label">Message:</label>
+                    <input id = "contact-form-message" class = "contact-form-input" type="text"/>
+
+                    <label class="contact-form-label"></label>
+                    <input id="contact-form-submit-button" type="submit"/>  
                 </form>
                 
             </section >
