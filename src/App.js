0c5f591fb7fb10676b3168d5bd81a204f1772848
yinzhedy <yinzhe.dy@gmail.com>
2023-06-15 17:50:17 -0400

    added blank pages for various collections, imported into app js and added links to route to each page when clicked on within dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Switch, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 // Components import
 import Footer from './components/footer/footer-index';
@@ -7,7 +7,9 @@ import Footer from './components/footer/footer-index';
 import Homepage from './pages/homepage/homepage-index';
 import About from './pages/about/about-index';
 import Contact from './pages/contact/contact-index';
-
+import PortraitCollection from './pages/portraits/portraits-index';
+import CustomCollection from './pages/custom/custom-index';
+import MiscCollection from './pages/misc/misc-index';
 
 function App() {
   return (
@@ -23,9 +25,9 @@ function App() {
           <div class="header-list-item dropdown">
             <a class="hover-mint">collections</a>
               <div id="collections-list" className="dropdown-content flex column left">
-                <a class="collections-list-item hover-mint">portraits</a>
-                <a class="collections-list-item hover-mint">fan art</a>
-                <a class="collections-list-item hover-mint">custom orders</a>
+                <a href ="/portrait" class="collections-list-item hover-mint">portraits</a>
+                <a href ="/misc" class="collections-list-item hover-mint">misc.</a>
+                <a href ="/custom" class="collections-list-item hover-mint">custom</a>
               </div>
 
           </div>
@@ -37,7 +39,10 @@ function App() {
         <Routes>
           <Route path="/" element = {<Homepage/>}/>
           <Route path="/about" element = {<About/>}/>
-          <Route path="contact" element ={<Contact/>}/>
+          <Route path="/contact" element ={<Contact/>}/>
+          <Route path="/portrait" element ={<PortraitCollection/>}/>
+          <Route path="/misc" element ={<MiscCollection/>}/>
+          <Route path="/custom" element ={<CustomCollection/>}/>
         </Routes>
       </BrowserRouter>
 
---
+++ src/pages/custom/custom-index.js
@@ -0,0 +1,13 @@
+const CustomCollection = () => {
+    return (
+        <main class="flex rows">
+        <div id="section-container" class="flex column nowrap justify-center align-center">
+            <section class = "section flex column nowrap justify-left align-left">
+                <h1 class="header-1">Custom Works</h1>
+            </section>
+        </div>
+    </main>
+    )
+}
+
+export default CustomCollection;
