bfc107db4282deb1a46574f661ec7fcae4e4b27b
yinzhedy <yinzhe.dy@gmail.com>
2023-06-15 17:10:01 -0400

    added basic components for contact page, including form and general contact info

--- src/pages/contact/contact-index.js
+++ src/pages/contact/contact-index.js
@@ -1,6 +1,28 @@
 const Contact = () => {
     return (
-        <div>Contact</div>
+        <main class="flex rows">
+        <div id="section-container" class="flex column nowrap justify-center align-center">
+            <section class = "section flex column nowrap justify-left align-left">
+                <h1>Contact Information and Links</h1>
+                <a> Github</a>
+                <a>LinkedIn</a>
+                <a>Email: yinzhe.dy@gmail.com</a>
+            </section>
+            <section class="section flex column nowrap">
+                <form class ="flex column nowrap justifu-left align-left">
+                  <h1>Contact Form</h1>
+                    <label>Name:</label>
+                    <input type="text"/>
+                    <label>Email:</label>
+                    <input type="email"/>
+                    <label>Message:</label>
+                    <input type="text"/>
+                    <input id="contact-submit-button" type="submit"/>  
+                </form>
+                
+            </section >
+        </div>
+    </main>
     )
 }
 
