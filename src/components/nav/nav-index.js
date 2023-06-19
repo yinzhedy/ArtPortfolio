e5152fcc95b730a4e98c1c13f452c9a25b31e8a7
yinzhedy <yinzhe.dy@gmail.com>
2023-06-19 17:01:55 -0400

    changed color theme to black and white, section background tbd

--- src/components/nav/nav-index.js
+++ src/components/nav/nav-index.js
@@ -3,9 +3,9 @@ const NavMenu = () => {
     return (
         <div>
             <ul class=" flex column wrap justify-left align-left"> 
-                <li  class = "nav-item"><a class = "style-link" href="/portrait">PORTRAITS</a></li>
-                <li  class = "nav-item"><a class = "style-link" href="/custom">CUSTOM WORKS</a></li>
-                <li  class = "nav-item"><a class = "style-link" href="/misc">MISC.</a></li>
+                <li  class = "nav-item"><a class = "style-link white-font" href="/portrait">PORTRAITS</a></li>
+                <li  class = "nav-item"><a class = "style-link white-font" href="/custom">CUSTOM WORKS</a></li>
+                <li  class = "nav-item"><a class = "style-link white-font" href="/misc">MISC.</a></li>
             </ul>
 
         </div>
--- src/pages/contact/contact-index.js
+++ src/pages/contact/contact-index.js
@@ -21,21 +21,21 @@ const Contact = () => {
     return (
         <div id="section-container" class="flex column nowrap justify-center align-center">
             <section class = "section flex column nowrap justify-left align-left">
-                <h1 class="header-1">Contact Information and Links</h1>
-                <a href="https://github.com/yinzhedy" class="style-link hover-mint-dark"> Github</a>
-                <a href="https://www.linkedin.com/in/yinzhe-dong-ydeen-7a116121b/" class="style-link hover-mint-dark">LinkedIn</a>
-                <a>Email: yinzhe.dy@gmail.com</a>
+                <h1 class="header-1 white-font">CONTACT INFO. & LINKS</h1>
+                <a href="https://github.com/yinzhedy" class="style-link hover-mint-dark white-font"> Github</a>
+                <a href="https://www.linkedin.com/in/yinzhe-dong-ydeen-7a116121b/" class="style-link hover-mint-dark white-font">LinkedIn</a>
+                <a class="white-font" >Email: yinzhe.dy@gmail.com</a>
             </section>
             <section id="contact-form-section" class="section flex column nowrap">
                 <form ref={form} onSubmit={sendEmail} id= "contact-form" class ="flex column nowrap justify-center align-center">
-                  <h1 class="header-1">Contact Form</h1>
-                    <label for='name-input' class= "contact-form-label">Name:</label>
-                    <input name="user_name" id="name-input" class = "contact-form-input" type="text"/>
+                    <h1 class="header-1">Contact Form</h1>
+                    <label for='name-input' class= "contact-form-label white-font">NAME:</label>
+                    <input name="user_name" id="name-input" class = "contact-form-input white-font" type="text"/>
 
-                    <label for="email-input" class="contact-form-label">Email:</label>
+                    <label for="email-input" class="contact-form-label white-font">EMAIL:</label>
                     <input name="user_email" id="email-input" class = "contact-form-input" type="email"/>
 
-                    <label for="contact-form-message" class="contact-form-label">Message:</label>
+                    <label for="contact-form-message" class="contact-form-label white-font">MESSAGE:</label>
                     <textarea name="message" id = "contact-form-message" class = "contact-form-input" type="text"/>
                     
                     <input value="Send" id="contact-form-submit-button" type="submit"/>  
